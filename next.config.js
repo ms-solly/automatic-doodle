f0b512f35c02f7ac300878d121cdc44c4327ac14
aditi-jogdand <theworldofthoughtsyo@gmail.com>
2024-07-29 23:27:39 +0530

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,21 +1,25 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
 	images: {
-		remotePatterns: [
-			{
-				protocol: "https",
-				hostname: "images.unsplash.com",
-			},
-			{
-				protocol: "https",
-				hostname: "source.unsplash.com",
-			},
-			{
-				protocol: "https",
-				hostname: "avatars.githubusercontent.com",
-			},
-		],
+	  remotePatterns: [
+		{
+		  protocol: "https",
+		  hostname: "images.unsplash.com",
+		},
+		{
+		  protocol: "https",
+		  hostname: "source.unsplash.com",
+		},
+		{
+		  protocol: "https",
+		  hostname: "avatars.githubusercontent.com",
+		},
+	  ],
 	},
-};
-
-module.exports = nextConfig;
+	experimental: {
+	  serverActions: true,
+	},
+  };
+  
+  module.exports = nextConfig;
+  
